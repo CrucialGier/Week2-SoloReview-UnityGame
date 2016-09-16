11ac341159b4e2bb3ccac811ed974dbb600911c2
Stewart Cole <stewart@>
2016-09-16 13:25:52 -0700

    add files and game so far to showcase file for submit

---
+++ scripts/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+    public BoardManager boardScript;
+    public int PlayerFoddPoints = 100;
+
+    [HideInInspector]
+    public bool PlayerTurn = true;
+
+    private int level = 3;
+	// Use this for initialization
+	void Awake ()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        } else if (instance != this )
+        {
+            Destroy(this);
+        }
+        DontDestroyOnLoad(gameObject);
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+	}
+
+    void InitGame()
+    {
+        boardScript.SetupScene(level);
+    }
+
+    public void GameOver()
+    {
+        enabled = false;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
